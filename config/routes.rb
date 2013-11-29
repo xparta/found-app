ce48cd495b8979aaa4819428ca0bd8aa4adfc644
xparta <tomarobaski@gmail.com>
2013-11-29 15:22:36 +0300

    ater hardships,generated home controller,and added bootsrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Foundapp::Application.routes.draw do
+ get "home" => "pages#home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
