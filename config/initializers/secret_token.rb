548ade05b5911f2048688351febea69d2e2de690
xparta <tomarobaski@gmail.com>
2013-11-29 14:40:46 +0300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Foundapp::Application.config.secret_key_base = '045fa4f1e55e84625bfd507926bf9df2b0355a8f0f2616001536a60b22c6b51f243e881c508b06a5199a039a5bcb0039da6a409abcbc1ecf3dbfaceb4b4783bc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Foundapp::Application.config.session_store :cookie_store, key: '_foundapp_session'
